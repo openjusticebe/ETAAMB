962fabf206af829937047b01b12ae11bf44898fc
pmontens <pieterjan@montens.net>
2021-10-18 21:23:48 +0200

    :wrench: update parsing code

--- agent/moniteur_import/Page.pm
+++ agent/moniteur_import/Page.pm
@@ -665,14 +665,15 @@ sub getPromDate
         januari=>1,februari=>2,maart=>3,april=>4,mei=>5,juni=>6,juli=>7,augustus=>8,september=>9,oktober=>10,november=>11,december=>12
         );
     $self->{_content} =~ m/<h3><center><u>\s(\d{1,2})\s(\w{1,})\s(\d{4})\./i;
-    if ($3 && $2 && $1)
-	{
-	return $3."-".$months{$2}."-".$1;
-	}
+
+    if ($3 && $2 && $1 && exists($months{$2}))
+        {
+        return $3."-".$months{$2}."-".$1;
+        }
     else
-	{
-	return '--';
-	}
+        {
+        return '--';
+        }
     }
 
 sub getType
