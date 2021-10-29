1faba7351102d20b339f3981764c39ebb38325f6
pmontens <pieterjan@montens.net>
2021-10-29 15:23:09 +0200

    :whale: prod docker image fix

--- etaamb/config.docker.php
+++ etaamb/config.docker.php
@@ -1,9 +1,11 @@
 <?php
-// Etaamb Docker config file
+// Etaamb Prod Docker config file
+// The environment variables in this file should be configured on the host
+// See Dockerfile for more build-time required arguments
 define("LANG_METHOD","url");
 define("ENVIRONMENT","prod");
 define("DOMAIN" , getenv('ETAAMB_DOMAIN'));
 define("ANON_HOST" , getenv('ETAAMB_ANON_HOST'));
 define("URL_PROTOCOL", getenv('ETAAMB_PROTOCOL'));
-
 define("URLMASK" , URL_PROTOCOL."://%host/%ln%page");
+define('MAIL_ADMIN', getenv('ADMIN_MAIL'));
